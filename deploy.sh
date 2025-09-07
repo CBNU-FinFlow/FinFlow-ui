7ef484863f50232c2c0858673677a4573a72f646
reo91004 <reo91004@gmail.com>
2025-09-08 08:17:29 +0900

    fix: 배포 시 .ts 확장자 문제로 인한 .mjs로 변경

--- deploy.sh
+++ deploy.sh
@@ -28,7 +28,7 @@ npm run build
 
 # 3. 압축
 echo "📦 압축 중..."
-tar -czf build.tar.gz .next public package.json package-lock.json next.config.ts
+tar -czf build.tar.gz .next public package.json package-lock.json next.config.mjs
 
 # 4. 업로드
 echo "📤 업로드 중..."
